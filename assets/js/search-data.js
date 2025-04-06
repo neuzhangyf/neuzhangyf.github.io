196ecd55e1e5e1c42f193d19158cd3ce9d06b9aa
neuzhangyf <34983318+neuzhangyf@users.noreply.github.com>
2025-04-06 10:32:15 +0000

    Deploying to gh-pages from @ neuzhangyf/neuzhangyf.github.io@15782ecf14cf4b24e80ca0d70a0fa6e5f6d2e0ca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -463,25 +463,11 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/neutronrag/";
             },},{
-        id: 'social-email',
-        title: 'email',
+        id: 'social-acm',
+        title: 'ACM DL',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%7A%68%61%6E%67%79%66@%6D%61%69%6C.%6E%65%75.%65%64%75.%63%6E", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=Bw90kcYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://idc-neu.github.io/", "_blank");
+          window.open("https://dl.acm.org/profile/81435603685/", "_blank");
         },
       },{
       id: 'light-theme',
