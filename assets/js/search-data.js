40328c6bef81c48bece62d1b51f3f6f8cb3fde13
neuzhangyf <34983318+neuzhangyf@users.noreply.github.com>
2025-04-06 12:56:15 +0000

    Deploying to gh-pages from @ neuzhangyf/neuzhangyf.github.io@369a34a70ecf585ef0fb9a4833c948dbf2535c2c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -509,28 +509,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("", "_blank");
-        },
-      },{
-        id: 'social-logo',
-        title: 'Logo',
-        section: 'Socials',
-        handler: () => {
-          window.open("", "_blank");
-        },
-      },{
-        id: 'social-title',
-        title: 'Title',
-        section: 'Socials',
-        handler: () => {
-          window.open("", "_blank");
-        },
-      },{
-        id: 'social-url',
-        title: 'Url',
-        section: 'Socials',
-        handler: () => {
-          window.open("", "_blank");
+          window.open("https://idc-neu.github.io/", "_blank");
         },
       },{
       id: 'light-theme',
